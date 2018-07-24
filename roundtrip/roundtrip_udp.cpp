8a84e729b3925141ca1015a9d6682b2ea60e7546
dingjiayi <1548534563@qq.com>
2018-07-24 19:41:10 +0800

    second

--- roundtrip/roundtrip_udp.cpp
+++ roundtrip/roundtrip_udp.cpp
@@ -1,6 +1,17 @@
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
 const size_t frameLen = 2*sizeof(int64_t);
 int createNonblockingUDP()
 {
-    
+    int sockfd = socket(PF_INET, SOCK_DGRAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
+    if (sockfd < 0)
+    {
+        std::cerr << "create nonblock udp socket failed\n"; 
+    }
+
+    return sockfd;
 }
+
+void serverRead
