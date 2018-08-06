6a74753ffb67c9d9bfa18bba146353c866a54b75
dingjiayi <1548534563@qq.com>
2018-08-06 22:16:39 +0800

    添加设置sockfdde 接口

--- roundtrip/tcp/SocketsOps.cpp
+++ roundtrip/tcp/SocketsOps.cpp
@@ -1,5 +1,7 @@
 
 #include "SocketsOps.h"
+#include <fcntl.h>
+#include <unistd.h>
 
 using SA = struct sockaddr;
 
@@ -13,3 +15,17 @@ SA* sockaddr_cast(struct sockaddr_in* addr)
     return static_cast<SA*> (implicit_cast<void*>(addr));
 }
 
+void setNonBlockAndCloseOnExec(int sockfd)
+{
+    int flags = fcntl(sockfd, FGETFL, 0);
+    flags |= O_NONBLOCK;
+    int ret = fcntl(sockfd, F_SETFL, flags);
+
+    flags = fcntl(sockfd, FGETFL, 0);
+    flags |= FD_CLOEXEC;
+    ret = fcntl(sockfd, F_SETFL, flags);
+
+    // 这个地方不能直接 flags |= O_NONBLOCK; flags |= FD_CLOEXEC
+    // 然后 一次调用 fcntl(sockfd, F_SETFL, flags)设置，这样是只设置了FD_CLOEXEC 和 O_NONBLOCK 
+    // 较大的那个（这个结论是实验获得，不代表所有都是这样）
+}
